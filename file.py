cfa60cc9839c57d6339999298730d815fa20f059
Iniyan-code <indiresh005@gmail.com>
2026-09-03 20:52:02 +0530

    create a file for saving notes create a dataset and practice EDA with pandas

--- file.py
+++ file.py
@@ -1,2 +1,9 @@
-print("indiresh");
-print("INDIESH")
+#exploratory data analysis
+import pandas as pd
+import pandas as pd
+
+df = pd.read_csv("dataset.csv")
+
+print(df.head())
+print(df.info())
+print(df.describe())
